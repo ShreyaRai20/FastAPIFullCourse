61140d4f10898d06a071831f6f5a4b3de462bc8c
ShreyaRai20 <shreya200199@gmail.com>
2025-04-11 23:23:33 -0500

    path and query parameter, together and optional

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
-
+# basic web server
 @app.get('/')
 async def read_root():
     return {'message': 'Hello World!'}
+
+# Path parameters - dynamic
+# @app.get('/greet/{name}')
+# async def greet_name(name: str) -> dict:
+#     return {'message': f'Hello {name}!'}
+
+# query parameter
+# /greet?name=shreya
+# @app.get('/greet')
+# async def greet_name(name: str) -> dict:
+#     return {'message': f'Hello {name}!'}
+
+
+# Path parameters + query parameter
+@app.get('/greet/{name}')
+async def greet_name(name: str, age:int) -> dict:
+    return {'message': f'Hello {name}! , with age {age}'}
+
+
+# Optional parameter - typing
+@app.get('/greet')
+async def greet_name(  age: int, name: Optional[str] = 'User') -> dict:
+    return {'message': f'Hello {name}! , with age {age}'}
